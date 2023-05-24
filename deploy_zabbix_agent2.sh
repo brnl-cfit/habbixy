ee2e677785dac74cf490354be2bdae5bcd0b33dd
Bram Mol <bram.mol@cfit.nl>
2023-05-24 14:33:27 +0200

    Added zabbix agent2 deploy script

---
+++ deploy_zabbix_agent2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+SOURCE_DIR=$(dirname $0)
+ZABBIX_DIR=/etc/zabbix
+
+mkdir -p ${ZABBIX_DIR}/scripts/agentd/habbixy
+cp ${SOURCE_DIR}/habbixy/map.index ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp ${SOURCE_DIR}/habbixy/habbixy.conf.example ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp ${SOURCE_DIR}/habbixy/habbixy.sh ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp ${SOURCE_DIR}/habbixy/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agent2.d/habbixy.conf
