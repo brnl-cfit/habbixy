26930e623b083d645e49004e417f71c320749b78
Bram Mol <bram.mol@cfit.nl>
2023-05-24 14:50:10 +0200

    Made output verbose (hardcoded)

--- deploy_zabbix_agent2.sh
+++ deploy_zabbix_agent2.sh
@@ -2,8 +2,8 @@
 SOURCE_DIR=$(dirname $0)
 ZABBIX_DIR=/etc/zabbix
 
-mkdir -p ${ZABBIX_DIR}/scripts/agentd/habbixy
-cp ${SOURCE_DIR}/habbixy/map.index ${ZABBIX_DIR}/scripts/agentd/habbixy/
-cp ${SOURCE_DIR}/habbixy/habbixy.conf.example ${ZABBIX_DIR}/scripts/agentd/habbixy/
-cp ${SOURCE_DIR}/habbixy/habbixy.sh ${ZABBIX_DIR}/scripts/agentd/habbixy/
-cp ${SOURCE_DIR}/habbixy/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agent2.d/habbixy.conf
+mkdir -v -p ${ZABBIX_DIR}/scripts/agentd/habbixy
+cp -v ${SOURCE_DIR}/habbixy/map.index ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp -v ${SOURCE_DIR}/habbixy/habbixy.conf.example ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp -v ${SOURCE_DIR}/habbixy/habbixy.sh ${ZABBIX_DIR}/scripts/agentd/habbixy/
+cp -v ${SOURCE_DIR}/habbixy/zabbix_agentd.conf ${ZABBIX_DIR}/zabbix_agent2.d/habbixy.conf
